125ce0225a970d5810b8f6922428c740459e4dd9
basedcodesBen <benaya.andrias@gmail.com>
2025-01-10 15:53:15 +0700

    fixed a bug where npm run dev doesn't work

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.mjs
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './', // Set the root directory where your index.html resides
+  publicDir: 'public',
   server: {
     open: true, // Automatically opens the browser
   },
